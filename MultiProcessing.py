552ecb7654a5092f0d3ca45ebbca9c4176bd96ed
Voyeux Alfred <alfredvoyeux@hotmail.com>
2024-05-30 13:12:32 +0200

    added a .gitignore and fixed the MultiProcessing.Pool_indexes() function.

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -1,7 +1,7 @@
-"""Has functions that help me when multiprocessing
+"""Has functions that help me when multiprocessing.
 """
 
-from numpy import ceil
+import numpy as np
 from typeguard import typechecked
 
 
@@ -12,10 +12,22 @@ class MultiProcessing:
     @typechecked
     @staticmethod
     def Pool_indexes(data_length: int, nb_processes: int = 4) -> list[tuple[int, int]]:
-        """
-        Gives out a list of tuples with the start and last data index for each process.
-        """
+        """Gives out a list of tuples with the start and last data index for each process.
 
-        # Step per process
-        step = int(ceil(data_length / nb_processes))
-        return [(step * i, step * (i + 1) - 1 if i != nb_processes - 1 else data_length - 1) for i in range(nb_processes)]
+        Args:
+            data_length (int): the length of the data that you want to multiprocess.
+            nb_processes (int, optional: the number or processes you want to run. If higher than data_length then it becomes data_length. Defaults to 4.
+
+        Returns:
+            list[tuple[int, int]]: the list of the start and end indexes for each process.
+        """
+        
+        if data_length > nb_processes:
+
+            # Step per process
+            step = data_length // nb_processes
+            leftover = data_length % nb_processes
+
+            return [((step * i) + min(i, leftover), step * (i + 1) + min(i + 1, leftover) - 1) for i in range(nb_processes)]
+        else:
+            return[(i, i) for i in range(data_length)]
--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .RE import *
-from .Plot import *
-from .common import *
-from .Decorators import *
-from .MultiProcessing import *
-from .ArrayManipulation import *

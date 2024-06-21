ec3c712956c044c4ed137d23b5044c3661ff706b
Voyeux Alfred <alfredvoyeux@hotmail.com>
2024-06-21 16:02:51 +0200

    forgot to add it to the commit.

---
+++ STEREO.py
@@ -0,0 +1,50 @@
+"""Stores some functions that are specific for STEREO data accessing.
+Right now it only reads the catalogue and reformats it to a pandas.DataFrame() object.
+"""
+
+# Imports
+import os
+import pandas as pd
+
+from typeguard import typechecked
+
+
+class StereoUtils:
+    
+    @staticmethod
+    def catalogue_path() -> str:
+        """Gives the full path of the STEREO catalogue. Only works when on the .ias.u-psud.fr server. Else, raises a ValueError.
+
+        Raises:
+            ValueError: when the path to the catalogue doesn't exist.
+
+        Returns:
+            str: the full path to the catalogue in Linux formatting.
+        """
+
+        path = '/archive/science_data/stereo/lz/L0/b/summary/sccB201207.img.eu'
+        if not os.path.exists(path): raise ValueError('Catalogue not found. You need to be connected to the .ias.u-psud.fr server to access the archive.')
+        return path
+    
+    @typechecked
+    @staticmethod
+    def read_catalogue(filter_compressed_data: bool = True) -> pd.DataFrame:
+        """To read the STEREO catalogue and output the corresponding pandas.DataFrame object.
+
+        Args:
+            filter_compressed_data (bool, optional): Choosing to filter the highly compressed data from the catalogue. From what I was told, these files
+            are so compressed that they are basically unusable. Defaults to True.
+
+        Returns:
+            pd.DataFrame: the pandas.DataFrame object corresponding to the STEREO catalogue. The headers are:
+            'FileName', 'DateObs', 'Tel', 'Exptime', 'Xsize', 'Ysize', 'Filter', 'Polar', 'Prog', 'OSnum', 'Dest', 'FPS', 'LED', 'CMPRS', 'NMISS'.
+        """
+        df = pd.read_csv(StereoUtils.catalogue_path(), delimiter='|', comment='=', engine='python')
+
+        # Remove trailing whitespaces in the column names and values
+        df.columns = [col.strip() for col in df.columns]
+        df = df.apply(lambda x: x.str.strip() if x.dtype == 'string' else x)
+
+        if filter_compressed_data: df = df[df['Dest'] != 'SW']
+        return df
+

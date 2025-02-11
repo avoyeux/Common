0eb47b9a30a6b0eb891a2a1b958f36ec639216ee
Voyeux Alfred <alfredvoyeux@hotmail.com>
2025-02-11 11:03:05 +0100

    MINOR: updated template to be less error prone.
    I added quotes and equal signs to make the bash executable less error prone.

--- common/batch_template.sh
+++ common/batch_template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Job name
-#SBATCH --job-name Rainbow
+#SBATCH --job-name="job_name"
 
 # Partition to use
-#SBATCH --partition all
+#SBATCH --partition=all
 ##SBATCH --exclusive
 
 # Resources
-##SBATCH --nodelist cluster-r7920-1,cluster-r7920-2,cluster-r730-k80-1
-#SBATCH --exclude cluster-r730-k80-1,cluster-r740-volta-1
-#SBATCH --nodes 1          # Number of nodes
-#SBATCH --mem 10Gb         # Memory
-#SBATCH --ntasks-per-node 4 
+##SBATCH --nodelist=cluster-r7920-1,cluster-r7920-2,cluster-r730-k80-1
+#SBATCH --exclude=cluster-r730-k80-1,cluster-r740-volta-1
+#SBATCH --nodes=1          # Number of nodes
+#SBATCH --mem=10Gb         # Memory
+#SBATCH --ntasks-per-node=4 
 
 # Work directory set as file directory
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
@@ -28,6 +28,6 @@ echo "job cpu per node = $SLURM_JOB_CPUS_PER_NODE"
 echo "Current working directory is `pwd`"
 
 source /home/avoyeux/.bashrc
-source ../.venv_dev/bin/activate
+source path/to/venv/bin/activate
 python3 --version
 python3 filename.py
